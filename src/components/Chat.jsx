8633b1afcecf167658c22498e35272cb46235e5c
Viviana Benitez <viviana.bernitez@gmail.com>
2023-07-20 09:47:35 -0500

    chat header and general style file added

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import "dotenv/config";
 import {
   useMultiChatLogic,
   MultiChatSocket,
   MultiChatWindow,
 } from "react-chat-engine-advanced";
+import { ChatHeaderCustom } from "./ChatHeaderCustom";
 
 const Chat = () => {
-  return <div className="Chat">Chat</div>;
+  const projectId = import.meta.env.CHAT_ENGINE_PROJECT_ID;
+  const username = "testUser1";
+  const secret = import.meta.env.CHAT_ENGINE_USER_SECRET;
+
+  const chatProps = useMultiChatLogic(projectId, username, secret);
+
+  return (
+    <div className="Chat" style={{ flexBasis: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat) => <ChatHeaderCustom chat={chat} />}
+      />
+    </div>
+  );
 };
 
 export { Chat };
